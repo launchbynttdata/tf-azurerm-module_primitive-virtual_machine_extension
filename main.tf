78091c941b19d4c0e192f896f0e634f471cc7d75
Aarti Joshi <aarti.joshi@nttdata.com>
2023-10-30 16:09:42 -0400

    Fix var

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@
 
 resource "azurerm_virtual_machine_extension" "virtual_machine_extension" {
   name                       = var.name
-  virtual_machine_id         = module.public_vm.id
+  virtual_machine_id         = var.virtual_machine_id
   publisher                  = var.publisher
   type                       = var.type
   type_handler_version       = var.type_handler_version
